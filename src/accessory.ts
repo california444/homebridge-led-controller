a762745e10fb23dda72451c63c14e9a175b09371
Roman Schlich <california444@gmail.com>
2021-02-07 21:37:12 +0100

    initial commit

---
+++ src/accessory.ts
@@ -0,0 +1,234 @@
+/**
+ * Copyright Roman Schlich
+ * Februar 2021
+ * Homebrigde Plugin for https://forum.fhem.de/index.php?topic=70738.0
+ * Update URL for new vbs SW: https://raw.githubusercontent.com/verybadsoldier/esp_rgbww_fhemmodule/master/controls_espledcontroller.txt
+ * API Spec: https://github.com/verybadsoldier/esp_rgbww_firmware/wiki/HTTP-Interface
+ */
+
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service
+} from "homebridge";
+import { request } from 'http';
+
+interface hsvData {
+    h: number;
+    s: number;
+    v: number;
+    ct: number;
+}
+
+interface requestObject {
+    hsv: hsvData;
+    cmd: string;
+    t: number;
+    q?: Boolean;
+    d?: Number;
+}
+
+interface getUpdate {
+  raw?: {
+    r: number;
+    g: number;
+    b: number;
+    ww: number;
+    cw:number;
+  }
+  hsv: hsvData;
+}
+
+let currentHsvState: hsvData = {h:0, s:0, v:0, ct:2700};
+
+let hap: HAP;
+
+/*
+ * Initializer function called when the plugin is loaded.
+ */
+export = (api: API) => {
+  hap = api.hap;
+  api.registerAccessory("Led-strip", LedLight);
+};
+
+class LedLight implements AccessoryPlugin {
+
+  private readonly log: Logging;
+  private readonly name: string;
+  private host: string;
+
+  private readonly ledService: Service;
+  private readonly informationService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name;
+    this.host = config.hostName;
+
+    this.ledService = new hap.Service.Lightbulb(this.name);
+
+    this.ledService.getCharacteristic(hap.Characteristic.Hue)
+    .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+      let hue = currentHsvState.h as number;
+      callback(undefined, hue);
+    })
+    .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+      currentHsvState.h = value as number;
+      this.sendUpdate();
+      callback();
+    });
+
+    this.ledService.getCharacteristic(hap.Characteristic.Saturation)
+    .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+      let sat = currentHsvState.s as number;
+      callback(undefined, sat);
+    })
+    .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+      currentHsvState.s = value as number;
+      this.sendUpdate();
+      callback();
+    });
+
+    this.ledService.getCharacteristic(hap.Characteristic.Brightness)
+    .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+      let brightness = currentHsvState.v as number;
+      callback(undefined, brightness);
+      
+    })
+    .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+      currentHsvState.v = value as number;
+      this.sendUpdate();
+      callback();
+    });
+
+    this.ledService.getCharacteristic(hap.Characteristic.ColorTemperature)
+    .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+      let ct = currentHsvState.ct as number;
+      callback(undefined, ct);
+      
+    })
+    .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+      currentHsvState.ct = value as number;
+      this.sendUpdate();
+      callback();
+    });
+
+    this.ledService.getCharacteristic(hap.Characteristic.On) 
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+
+        //this.getUpdate();
+        let state: boolean = currentHsvState.v > 0 ? true : false;
+        callback(undefined, state);
+
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        currentHsvState.v = value as boolean ? 100 :0;
+        this.sendUpdate();
+
+        callback();
+      });
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Selbstbau")
+      .setCharacteristic(hap.Characteristic.Model, "ESP RGBWW Wifi Led Controller");
+
+      const timer = setInterval(() => {
+        this.getUpdate();
+      }, 5000);
+
+    log.info("Switch finished initializing!");
+  }
+
+  cycleUpdate(): void {
+    
+  }
+
+  sendUpdate(): void {
+
+    const req = request(
+      {
+        host: this.host,
+        path: '/color',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        }
+      },
+      
+      response => {
+        let result;
+        const chunks: any = [];
+        response.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
+        response.on('end', () => {
+          result = Buffer.concat(chunks).toString();
+          this.log.debug("received result in post:"+result);
+        });
+      }
+    );
+    let requestData :requestObject = {hsv: currentHsvState, cmd:"fade", t:600};
+
+    this.log.debug("sending req:" +JSON.stringify(requestData));
+    req.write(JSON.stringify(requestData));
+    req.end();
+  }
+
+  getUpdate(): void {
+    let result: any;
+    const req = request(
+      {
+        host: this.host,
+        path: '/color',
+        method: 'GET',
+      },
+      response => {
+        const chunks: any = [];
+        response.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
+        response.on('end', () => {
+          result = Buffer.concat(chunks).toString();
+          this.log.debug("received result in get: " + result);
+          if(result) {
+            let obj : getUpdate = JSON.parse(result);
+            currentHsvState = obj.hsv;
+            this.ledService.updateCharacteristic(hap.Characteristic.On, currentHsvState.v > 0 ? true : false);
+            this.ledService.updateCharacteristic(hap.Characteristic.Brightness, currentHsvState.v as number);
+            this.ledService.updateCharacteristic(hap.Characteristic.Saturation, currentHsvState.s as number);
+            this.ledService.updateCharacteristic(hap.Characteristic.ColorTemperature, currentHsvState.ct as number);
+            this.ledService.updateCharacteristic(hap.Characteristic.Hue, currentHsvState.h as number);
+          }
+        });
+      }
+    );
+    req.end();
+  }
+
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.ledService,
+    ];
+  }
+
+}
